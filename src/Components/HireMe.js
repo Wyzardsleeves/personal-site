1a61363b37224f0afbc035dab801688562c107ce
Justin Lattimore <jlattimoreweb@gmail.com>
2020-10-21 11:16:26 -0500

    changed resume

--- src/Components/HireMe.js
+++ src/Components/HireMe.js
@@ -9,7 +9,7 @@ const HireMe = () => {
         <div className="resume">
           <a href={resumePdf} rel="noopener noreferrer" target="_blank" download><h5><i className="fas fa-file-pdf"></i> Download Resume</h5></a>
           <p>I work primarily with React.js on the front-end and Rails on the back-end but picking up new languages or frameworks isn't a problem. I'm available for hire on a freelance or contract basis. Why not send me an email to jlattimoreweb@gmail.com to discuss it further?</p>
-          <iframe title="hireme" src="https://resume.creddle.io/embed/a1lidyjttgc"  width="854" height="1104" ></iframe>
+          <iframe title="hireme" src={resumePdf} width="854" height="1135" ></iframe>
         </div>
       </div>
     )
